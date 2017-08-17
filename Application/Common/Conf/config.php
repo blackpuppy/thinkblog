e98a0e9209a6bd481bd0c99980544e4d343f13d2
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-17 15:42:57 +0800

    配置
    - 设置URL伪静态后缀为空，去掉URL中的后缀

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,6 +1,16 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //'配置项'=>'配置值'
+
+    // URL设置
     'URL_MODEL' => URL_REWRITE,
+    'URL_HTML_SUFFIX' => '',    // URL伪静态后缀设置
+    'URL_ROUTER_ON'   => true,  //开启路由
+    // 'URL_ROUTE_RULES' => array( //定义路由规则
+    //     '/'         => 'Index/index',
+    //     '/add'      => 'Index/add',
+    //     '/update'   => 'Index/update',
+    // ),
+
     // 'LAYOUT_ON' => true,
 );

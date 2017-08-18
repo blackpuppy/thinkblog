43fb5765fd1c100b5a482447566a26923a9c4712
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-18 22:09:16 +0800

    配置放入环境
    - 更多ThinkPHP配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,14 +3,12 @@ return array(
     //'配置项'=>'配置值'
 
     // URL设置
-    'URL_MODEL' => URL_REWRITE,
-    'URL_HTML_SUFFIX' => '',    // URL伪静态后缀设置
     'URL_ROUTER_ON'   => true,  //开启路由
-    // 'URL_ROUTE_RULES' => array( //定义路由规则
-    //     '/'         => 'Index/index',
-    //     '/add'      => 'Index/add',
-    //     '/update'   => 'Index/update',
-    // ),
+    'URL_ROUTE_RULES' => array( //定义路由规则
+        '/'             => 'Index/index',
 
-    // 'LAYOUT_ON' => true,
+        '/posts'        => 'Post/index',
+        '/posts/add'    => 'Post/add',
+        '/posts/update' => 'Post/update',
+    ),
 );
--- webroot/index.php
+++ webroot/index.php
@@ -18,11 +18,19 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
 
+// 引入composer
+require(dirname(__DIR__) . '/vendor/autoload.php');
+
+// 载入环境配置，供getenv()使用
+$config = (new josegonzalez\Dotenv\Loader(dirname(__DIR__) . '/.env'))
+              ->parse();
+$config->putenv(true);
+
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', true);
+define('APP_DEBUG', getenv('APP_DEBUG'));
 
 // 是否生成目录安全文件
-define('BUILD_DIR_SECURE', false);
+define('BUILD_DIR_SECURE',  getenv('BUILD_DIR_SECURE'));
 
 // 定义应用目录
 define('APP_PATH', '../Application/');
@@ -33,15 +41,17 @@ require '../vendor/topthink/thinkphp/ThinkPHP/ThinkPHP.php';
 // 亲^_^ 后面不需要任何代码了 就是如此简单
 
 $msg = PHP_EOL . 'index.php:'
-    . PHP_EOL . '路径常量:'
-    . PHP_EOL . '  THINK_VERSION = ' . THINK_VERSION
-    . PHP_EOL . '  THINK_PATH = ' . THINK_PATH
-    . PHP_EOL . '  LIB_PATH = ' . LIB_PATH
-    . PHP_EOL . '  CORE_PATH = ' . CORE_PATH
-    . PHP_EOL . '  APP_PATH = ' . APP_PATH
-    . PHP_EOL . '  LOG_PATH = ' . LOG_PATH
-    . PHP_EOL . '  DATA_PATH = ' . DATA_PATH
-    . PHP_EOL . '  APP_DEBUG = ' . APP_DEBUG
+    // . PHP_EOL . '  APP_DEBUG = ' . APP_DEBUG
+    // . PHP_EOL . '  BUILD_DIR_SECURE = ' . BUILD_DIR_SECURE
+    // . PHP_EOL . '路径常量:'
+    // . PHP_EOL . '  THINK_VERSION = ' . THINK_VERSION
+    // . PHP_EOL . '  THINK_PATH = ' . THINK_PATH
+    // . PHP_EOL . '  LIB_PATH = ' . LIB_PATH
+    // . PHP_EOL . '  CORE_PATH = ' . CORE_PATH
+    // . PHP_EOL . '  APP_PATH = ' . APP_PATH
+    // . PHP_EOL . '  LOG_PATH = ' . LOG_PATH
+    // . PHP_EOL . '  DATA_PATH = ' . DATA_PATH
+    // . PHP_EOL . '  APP_DEBUG = ' . APP_DEBUG
     . PHP_EOL . '系统常量:'
     . PHP_EOL . '  __APP__ = ' . __APP__
     . PHP_EOL . '  __MODULE__ = ' . __MODULE__
@@ -52,8 +62,8 @@ $msg = PHP_EOL . 'index.php:'
     . PHP_EOL . '  MODULE_NAME = ' . MODULE_NAME
     . PHP_EOL . '  CONTROLLER_NAME = ' . CONTROLLER_NAME
     . PHP_EOL . '  ACTION_NAME = ' . ACTION_NAME
-    . PHP_EOL . '  REQUEST_METHOD = ' . REQUEST_METHOD
-    . PHP_EOL . '  IS_AJAX = ' . IS_AJAX
+    // . PHP_EOL . '  REQUEST_METHOD = ' . REQUEST_METHOD
+    // . PHP_EOL . '  IS_AJAX = ' . IS_AJAX
     . PHP_EOL . '配置:'
     . PHP_EOL . '  C(URL_MODEL) = ' . C('URL_MODEL')
     . PHP_EOL . str_repeat('-', 80);

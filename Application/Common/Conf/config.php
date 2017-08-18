4148d4f5b215f64c666250f96619e2da3ea0e2eb
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-19 00:04:44 +0800

    基本的 CRUD
    - 改正路由

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,10 +5,10 @@ return array(
     // URL设置
     'URL_ROUTER_ON'   => true,  //开启路由
     'URL_ROUTE_RULES' => array( //定义路由规则
-        '/'             => 'Index/index',
+        // '/'             => 'Index/index',
 
-        '/posts'        => 'Post/index',
-        '/posts/add'    => 'Post/add',
-        '/posts/update' => 'Post/update',
+        'posts'        => 'Post/index',
+        'posts/add'    => 'Post/add',
+        'posts/update' => 'Post/update',
     ),
 );

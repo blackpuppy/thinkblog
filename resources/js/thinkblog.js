c5a9ba50c3695ae18e0fe0c13f2ade96a1705a95
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-20 17:46:48 +0800

    基本的 CRUD
    - 删除文章

--- resources/js/thinkblog.js
+++ resources/js/thinkblog.js
@@ -1,19 +1,31 @@
 (function($) {
     /**
-     * ThinkBlog namespace.
-     * All global function and variable must belong to this object.
+     * ThinkBlog 命名空间.
+     * 所有全局函数和变量都要放在这个对象中。
      */
     var ThinkBlog = function() {
     };
 
-    //Initialize global ThinkBlog object
+    // 初始化全局 ThinkBlog 对象
     window.ThinkBlog = new ThinkBlog();
 })(jQuery);
 
 $(document).ready(function() {
-    // Enable Boostrap Confirmation
+    // 启用 Boostrap Confirmation
     $('[data-toggle=confirmation]').confirmation({
         rootSelector: '[data-toggle=confirmation]',
         // other options
+    });
+
+    // 确认之后，删除文章
+    $('.post-listing [data-toggle=confirmation]').click(function (e) {
+        console.debug('.post-listing [data-toggle=confirmation] clicked:');
+
+        e.preventDefault();
+
+        var url = $(this).data('delete-url'),
+            $form = $('.post-listing .delete-post-form');
+        console.debug('  url = ', url);
+        $form.prop('action', url).submit();
     });
 });

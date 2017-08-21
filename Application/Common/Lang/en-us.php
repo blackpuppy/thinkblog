6c835ce573e4e78bdcce76ff1e918e8fea9e83cf
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-22 05:41:07 +0800

    多语言支持
    - 为一些页面添加多语言支持

---
+++ Application/Common/Lang/en-us.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    // application
+    'APPLICATION_NAME'          => 'ThinkPHP Blog',
+    'APPLICATION_SHORT_DESC'    => 'ThinkPHP Development Demo Application',
+
+    // home page
+    'HOMEPAGE_DESC'             => 'Demonstrates the following techniques:',
+    'REMOVE_ENTRY_IN_URL'       => 'Remove entry file {$entry_file} from URL',
+    'DEV_USING_DOCKER'          => 'Dockerize local development environment',
+    'CONFIG_IN_ENV'             => 'Externalize configuration in environment',
+    'BASIC_CRUD'                => 'Basic CRUD',
+    'BUILD_WITH_LARAVEL_MIX'    => 'Build frontend assets with Laravel Mix',
+    'USER_AUTH'                 => 'User Authentication/Authorization',
+    'MULTI_LANGUAGES'           => 'Internationalization/Localization (I18n/L10n)',
+    'WEB_API'                   => 'Web API',
+    'ANGULARJS_1_CLIENT'        => 'AngularJS 1 Client',
+
+    // menu
+    'MENU_POSTS'        => 'Posts',
+    'SWITCH_LANGUAGE'   => 'Language',
+    'CHINESE'           => 'Chinese',
+    'ENGLISH'           => 'English',
+
+    // footer
+    'WELCOME_TO_USE'    => 'Welcome to developing with',
+];
---
+++ Application/Common/Lang/zh-cn.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    // 应用程序
+    'APPLICATION_NAME'          => 'ThinkPHP 博客',
+    'APPLICATION_SHORT_DESC'    => '用 ThinkPHP 进行开发的演示程序',
+
+    // 主页
+    'HOMEPAGE_DESC'             => '演示以下技术：',
+    'REMOVE_ENTRY_IN_URL'       => '去掉地址里面的入口文件 {$entry_file}',
+    'DEV_USING_DOCKER'          => '用 Docker 配置本地开发环境',
+    'CONFIG_IN_ENV'             => '配置放入环境',
+    'BASIC_CRUD'                => '基本的 CRUD',
+    'BUILD_WITH_LARAVEL_MIX'    => '用 Laravel Mix 构建前端资源',
+    'USER_AUTH'                 => '用户验证与授权',
+    'MULTI_LANGUAGES'           => '多语言支持',
+    'WEB_API'                   => 'Web API',
+    'ANGULARJS_1_CLIENT'        => 'AngularJS 1 客户端',
+
+    // 菜单
+    'MENU_POSTS'        => '文章',
+    'SWITCH_LANGUAGE'   => '语言',
+    'CHINESE'           => '中文',
+    'ENGLISH'           => '英文',
+
+    // 页脚
+    'WELCOME_TO_USE'    => '欢迎使用',
+];

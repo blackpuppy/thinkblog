9a218824ada14ea1951669b4ccb22f2f0afd3f3c
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-24 14:21:00 +0800

    用户授权
    - Auth授权的配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -30,4 +30,17 @@ return [
         'posts/delete/:id\d'    => 'Post/delete',
         'posts'                 => 'Post/index',
     ],
+
+    'AUTH_CONFIG' => [
+        'AUTH_ON' => getenv('AUTH_ON'),                         // 认证开关
+        'AUTH_TYPE' => getenv('AUTH_TYPE'),                     // 认证方式，1为实时认证；2为登录认证。
+        'AUTH_GROUP' =>
+            getenv('DB_PREFIX') . getenv('AUTH_GROUP'),         // 用户组数据表名
+        'AUTH_GROUP_ACCESS' =>
+            getenv('DB_PREFIX') . getenv('AUTH_GROUP_ACCESS'),  // 用户-用户组关系表
+        'AUTH_RULE' =>
+            getenv('DB_PREFIX') . getenv('AUTH_RULE'),          // 权限规则表
+        'AUTH_USER' =>
+            getenv('DB_PREFIX') . getenv('AUTH_USER'),          // 用户信息表
+    ],
 ];

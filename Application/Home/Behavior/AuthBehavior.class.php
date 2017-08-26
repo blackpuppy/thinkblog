1e46ed679d49e0e5114de9acf1c1773ff0009bc1
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-27 00:37:36 +0800

    用户认证与授权
    - 修复死循环，登录之后也需要考虑是否为公共页面

--- Application/Home/Behavior/AuthBehavior.class.php
+++ Application/Home/Behavior/AuthBehavior.class.php
@@ -8,11 +8,11 @@ use Think\Behavior;
  * 检查用户认证与授权的行为。
  */
 class AuthBehavior extends Behavior {
-    protected $msg = '';
-
     public function run(&$return)
     {
-        $this->msg = PHP_EOL . 'Home\Behavior\AuthBehavior::run():'
+        $return = true;
+
+        $msg = PHP_EOL . 'Home\Behavior\AuthBehavior::run():'
             . PHP_EOL . '系统常量:'
             . PHP_EOL . '  __APP__ = ' . (defined('__APP__') ? __APP__ : '')
             . PHP_EOL . '  __MODULE__ = ' . (defined('__MODULE__') ? __MODULE__ : '')
@@ -27,101 +27,96 @@ class AuthBehavior extends Behavior {
             . PHP_EOL . '配置:'
             . PHP_EOL . '  C(AUTH_CONFIG.AUTH_ON) = ' . C('AUTH_CONFIG.AUTH_ON');
 
-        if (!C('AUTH_CONFIG.AUTH_ON')) {
-            $return = true;
-        } else {
-            $return = $this->checkAuth();
-        }
-
-        $this->msg .= PHP_EOL . '  $return = ' . $return;
-        $this->msg .= PHP_EOL . str_repeat('-', 80);
-        // \Think\Log::write($this->msg, 'DEBUG');
-        trace($this->msg, '调试', 'DEBUG', true);
-    }
-
-    protected function checkAuth()
-    {
-        $return = true;
-
-        $this->msg .= PHP_EOL . '  session() = ' . print_r(session(), true);
+        $msg .= PHP_EOL . '  session() = ' . print_r(session(), true);
 
         $isAuthenticated = session('?authentication.authenticated')
                         && session('authentication.authenticated');
 
         $url = MODULE_NAME . '/' . CONTROLLER_NAME . '/' . ACTION_NAME;
         $loginUrl = U(C('AUTH_CONFIG.AUTH_LOGIN_URL'));
+        $loginRedirectUrl = U(C('AUTH_CONFIG.AUTH_LOGIN_REDIRECT_URL'));
+        $isPublicUrl = $this->checkPublicUrl($url);
 
-        $this->msg .= PHP_EOL . '  $isAuthenticated = ' . $isAuthenticated
+        $msg .= PHP_EOL . '  $isAuthenticated = ' . $isAuthenticated
             . PHP_EOL . '  $url = ' . $url
-            . PHP_EOL . '  $loginUrl = ' . $loginUrl;
+            . PHP_EOL . '  $loginUrl = ' . $loginUrl
+            . PHP_EOL . '  $loginRedirectUrl = ' . $loginRedirectUrl
+            . PHP_EOL . '  $isPublicUrl = ' . $isPublicUrl;
 
-        if ($isAuthenticated) {
-            $this->msg .= PHP_EOL . '  member:';
+        if (!$isPublicUrl) {
+            if ($isAuthenticated) {
+                $msg .= PHP_EOL . '  member:';
 
-            if (C('AUTH_CONFIG.AUTH_ON')) {
-                $userId = session('authentication.user')['id'];
+                if (C('AUTH_CONFIG.AUTH_ON')) {
+                    $userId = session('authentication.user')['id'];
 
-                $Auth = new Auth();
-                $isAuthorized = $Auth->check($url, $userId);
+                    $Auth = new Auth();
+                    $isAuthorized = $Auth->check($url, $userId);
 
-                $this->msg .= PHP_EOL . '  $userId = ' . $userId
-                    . PHP_EOL . '  $isAuthorized = ' . $isAuthorized;
+                    $msg .= PHP_EOL . '  $userId = ' . $userId
+                        . PHP_EOL . '  $isAuthorized = ' . $isAuthorized;
 
-                if (!$isAuthorized) {
+                    if (!$isAuthorized) {
+                        $return = false;
+
+                        $msg .= PHP_EOL . '  $return = ' . $return;
+                        $msg .= PHP_EOL . str_repeat('-', 80);
+                        \Think\Log::write($msg, 'DEBUG');
+
+                        // send_http_status(401);
+
+                        // $this->error(L('UNAUTHORIZED'), $loginUrl, 5);
+                        // redirect($loginUrl, 5, L('UNAUTHORIZED'));
+                        redirect($loginRedirectUrl);
+                    }
+                }
+
+                // $return = !C('AUTH_CONFIG.AUTH_ON') || $isAuthorized;
+            } else {
+                $msg .= PHP_EOL . '  public user:';
+
+                if (!$isPublicUrl) {
                     $return = false;
 
-                    $this->msg .= PHP_EOL . '  $return = ' . $return;
-                    $this->msg .= PHP_EOL . str_repeat('-', 80);
+                    $msg .= PHP_EOL . '  $return = ' . $return;
+                    $msg .= PHP_EOL . str_repeat('-', 80);
+                    \Think\Log::write($msg, 'DEBUG');
 
                     // send_http_status(401);
 
                     // $this->error(L('UNAUTHORIZED'), $loginUrl, 5);
                     // redirect($loginUrl, 5, L('UNAUTHORIZED'));
                     redirect($loginUrl);
-                } else {
-                    $return = true;
                 }
+
+                // $return = !$isAuthenticated && $isPublicUrl;
             }
-
-            // $return = !C('AUTH_CONFIG.AUTH_ON') || $isAuthorized;
-        } else {
-            $this->msg .= PHP_EOL . '  public user:';
-
-            $where = array(
-                'type'   => 1,
-                'status' => 1,
-            );
-            $rules = M()->table(C('AUTH_CONFIG.AUTH_RULE'))
-                ->where($where)->field('name')->select();
-
-            array_walk($rules, function (&$rule)
-            {
-                $rule = $rule['name'];
-            });
-
-            $isPublicUrl = !in_array($url, $rules);
-
-            $this->msg .= PHP_EOL . '  $rules = ' . print_r($rules, true)
-                . PHP_EOL . '  $isPublicUrl = ' . $isPublicUrl;
-
-            if (!$isPublicUrl) {
-                $return = false;
-
-                $this->msg .= PHP_EOL . '  $return = ' . $return;
-                $this->msg .= PHP_EOL . str_repeat('-', 80);
-
-                // send_http_status(401);
-
-                // $this->error(L('UNAUTHORIZED'), $loginUrl, 5);
-                // redirect($loginUrl, 5, L('UNAUTHORIZED'));
-                redirect($loginUrl);
-            } else {
-                $return = true;
-            }
-
-            // $return = !$isAuthenticated && $isPublicUrl;
         }
 
-        return $return;
+        $msg .= PHP_EOL . '  $return = ' . $return;
+        $msg .= PHP_EOL . str_repeat('-', 80);
+        \Think\Log::write($msg, 'DEBUG');
+        // trace($msg, '调试', 'DEBUG', true);
+    }
+
+    protected function checkPublicUrl($url)
+    {
+        $where = array(
+            'type'   => 1,
+            'status' => 1,
+        );
+        $rules = M()->table(C('AUTH_CONFIG.AUTH_RULE'))
+            ->where($where)->field('name')->select();
+
+        array_walk($rules, function (&$rule)
+        {
+            $rule = $rule['name'];
+        });
+
+        // $msg .= PHP_EOL . '  $rules = ' . print_r($rules, true)
+
+        $isPublicUrl = !in_array($url, $rules);
+
+        return $isPublicUrl;
     }
 }

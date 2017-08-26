4d8b075c6e40e5f2311477ee181530dd533055a4
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-26 23:32:53 +0800

    用户认证与授权
    - 根据认证开关，决定是否检查用户认证与授权

--- Application/Home/Behavior/AuthBehavior.class.php
+++ Application/Home/Behavior/AuthBehavior.class.php
@@ -8,10 +8,42 @@ use Think\Behavior;
  * 检查用户认证与授权的行为。
  */
 class AuthBehavior extends Behavior {
+    protected $msg = '';
 
     public function run(&$return)
     {
-        $msg = PHP_EOL . 'Home\Behavior\AuthBehavior::run():';
+        $this->msg = PHP_EOL . 'Home\Behavior\AuthBehavior::run():'
+            . PHP_EOL . '系统常量:'
+            . PHP_EOL . '  __APP__ = ' . (defined('__APP__') ? __APP__ : '')
+            . PHP_EOL . '  __MODULE__ = ' . (defined('__MODULE__') ? __MODULE__ : '')
+            . PHP_EOL . '  __CONTROLLER__ = ' . (defined('__CONTROLLER__') ? __CONTROLLER__ : '')
+            . PHP_EOL . '  __ACTION__ = ' . (defined('__ACTION__') ? __ACTION__ : '')
+            . PHP_EOL . '  __SELF__ = ' . (defined('__SELF__') ? __SELF__ : '')
+            . PHP_EOL . '  __INFO__ = ' . __INFO__
+            . PHP_EOL . '  MODULE_NAME = ' . MODULE_NAME
+            . PHP_EOL . '  CONTROLLER_NAME = ' . (defined('CONTROLLER_NAME') ? CONTROLLER_NAME : '')
+            . PHP_EOL . '  ACTION_NAME = ' . (defined('ACTION_NAME') ? ACTION_NAME : '')
+            . PHP_EOL . '  REQUEST_METHOD = ' . (defined('REQUEST_METHOD') ? REQUEST_METHOD : '')
+            . PHP_EOL . '配置:'
+            . PHP_EOL . '  C(AUTH_CONFIG.AUTH_ON) = ' . C('AUTH_CONFIG.AUTH_ON');
+
+        if (!C('AUTH_CONFIG.AUTH_ON')) {
+            $return = true;
+        } else {
+            $return = $this->checkAuth();
+        }
+
+        $this->msg .= PHP_EOL . '  $return = ' . $return;
+        $this->msg .= PHP_EOL . str_repeat('-', 80);
+        // \Think\Log::write($this->msg, 'DEBUG');
+        trace($this->msg, '调试', 'DEBUG', true);
+    }
+
+    protected function checkAuth()
+    {
+        $return = true;
+
+        $this->msg .= PHP_EOL . '  session() = ' . print_r(session(), true);
 
         $isAuthenticated = session('?authentication.authenticated')
                         && session('authentication.authenticated');
@@ -19,12 +51,12 @@ class AuthBehavior extends Behavior {
         $url = MODULE_NAME . '/' . CONTROLLER_NAME . '/' . ACTION_NAME;
         $loginUrl = U(C('AUTH_CONFIG.AUTH_LOGIN_URL'));
 
-        $msg .= PHP_EOL . '  $isAuthenticated = ' . $isAuthenticated
+        $this->msg .= PHP_EOL . '  $isAuthenticated = ' . $isAuthenticated
             . PHP_EOL . '  $url = ' . $url
             . PHP_EOL . '  $loginUrl = ' . $loginUrl;
 
         if ($isAuthenticated) {
-            $msg .= PHP_EOL . '  member:';
+            $this->msg .= PHP_EOL . '  member:';
 
             if (C('AUTH_CONFIG.AUTH_ON')) {
                 $userId = session('authentication.user')['id'];
@@ -32,7 +64,15 @@ class AuthBehavior extends Behavior {
                 $Auth = new Auth();
                 $isAuthorized = $Auth->check($url, $userId);
 
+                $this->msg .= PHP_EOL . '  $userId = ' . $userId
+                    . PHP_EOL . '  $isAuthorized = ' . $isAuthorized;
+
                 if (!$isAuthorized) {
+                    $return = false;
+
+                    $this->msg .= PHP_EOL . '  $return = ' . $return;
+                    $this->msg .= PHP_EOL . str_repeat('-', 80);
+
                     // send_http_status(401);
 
                     // $this->error(L('UNAUTHORIZED'), $loginUrl, 5);
@@ -42,8 +82,10 @@ class AuthBehavior extends Behavior {
                     $return = true;
                 }
             }
+
+            // $return = !C('AUTH_CONFIG.AUTH_ON') || $isAuthorized;
         } else {
-            $msg .= PHP_EOL . '  public user:';
+            $this->msg .= PHP_EOL . '  public user:';
 
             $where = array(
                 'type'   => 1,
@@ -59,10 +101,15 @@ class AuthBehavior extends Behavior {
 
             $isPublicUrl = !in_array($url, $rules);
 
-            $msg .= PHP_EOL . '  $rules = ' . print_r($rules, true)
+            $this->msg .= PHP_EOL . '  $rules = ' . print_r($rules, true)
                 . PHP_EOL . '  $isPublicUrl = ' . $isPublicUrl;
 
             if (!$isPublicUrl) {
+                $return = false;
+
+                $this->msg .= PHP_EOL . '  $return = ' . $return;
+                $this->msg .= PHP_EOL . str_repeat('-', 80);
+
                 // send_http_status(401);
 
                 // $this->error(L('UNAUTHORIZED'), $loginUrl, 5);
@@ -71,10 +118,10 @@ class AuthBehavior extends Behavior {
             } else {
                 $return = true;
             }
+
+            // $return = !$isAuthenticated && $isPublicUrl;
         }
 
-        $msg .= PHP_EOL . '  $return = ' . $return;
-        $msg .= PHP_EOL . str_repeat('-', 80);
-        \Think\Log::write($msg, 'DEBUG');
+        return $return;
     }
 }

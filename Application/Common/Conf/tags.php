76f4a7a99c51f6633cc48ca912cca846a8e1831b
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-26 23:30:22 +0800

    用户授权
    - 延迟到action_begin标签才调用AuthBehavior行为，检查授权

--- Application/Common/Conf/tags.php
+++ Application/Common/Conf/tags.php
@@ -3,10 +3,28 @@
 return [
     'app_init' => [
         'Snowair\Think\Behavior\HookAgent',
+        // 'Home\Behavior\AuthBehavior',   // too early, constants not set
     ],
+
+    // 'module_check' => [
+    //     'Home\Behavior\DebugBehavior',
+    // ],
+
+    // 'path_info' => [
+    //     'Home\Behavior\DebugBehavior',
+    // ],
+
+    // 'url_dispatch' => [
+    //     'Home\Behavior\DebugBehavior',
+    //     // 'Home\Behavior\AuthBehavior',
+    // ],
 
     'app_begin' => [
         'Behavior\CheckLangBehavior',
+        // 'Home\Behavior\AuthBehavior',   // too late, route changed
+    ],
+
+    'action_begin' => [
         'Home\Behavior\AuthBehavior',
     ],
 ];

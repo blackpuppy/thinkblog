f83e2124260db150607d82c65d0786df2c7f4f01
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-31 02:38:31 +0800

    Azure Website
    - 调试

--- deploy.sh
+++ deploy.sh
@@ -136,10 +136,11 @@ fi
 
 # 2.2 Verify composer installed
 cd "$DEPLOYMENT_TARGET"
-pushd "$DEPLOYMENT_TARGET"
+# pushd "$DEPLOYMENT_TARGET"
+cd
 hash composer 2>/dev/null
 exitWithMessageOnError "Missing composer executable"
-popd
+# popd
 cd - > /dev/null
 
 # 2.3 Initialize Composer Config
@@ -151,6 +152,7 @@ if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
   echo "Found composer.json"
   cd "$DEPLOYMENT_TARGET"
   # pushd "$DEPLOYMENT_TARGET"
+  cd
   composer install $COMPOSER_ARGS
   exitWithMessageOnError "Composer install failed"
   # popd

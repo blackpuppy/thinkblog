402bb84c7818742610da27665daa26198cb92b66
Zhu Ming <mingzhu.z+github@gmail.com>
2017-08-31 02:50:55 +0800

    Azure Website
    - 调试

--- deploy.sh
+++ deploy.sh
@@ -124,6 +124,7 @@ fi
 if [ -e "$DEPLOYMENT_TARGET/composer.phar" ]; then
   echo "**** Installing composer ****"
   pushd "$DEPLOYMENT_TARGET"
+  pwd
 
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
   php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"

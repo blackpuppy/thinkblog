ae3fb4cb05b067133e10703fbb9f58edcce76ee1
Zhu Ming <mingzhu.z+github@gmail.com>
2017-09-01 13:03:17 +0800

    Azure Website
    - 调整前端资源的构建

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,30 +1,30 @@
 const mix = require('laravel-mix');
 const path = require('path');
 
-mix.webpackConfig({
-    output: {
-        path: path.resolve(__dirname, 'webroot/Public'),
-        pathinfo: process.env.NODE_ENV !== 'production'
-        // publicPath: path.normalize('webroot/Public')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(woff2?|ttf|eot|svg|otf)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]?[hash]',
-                    outputPath: '/fonts/',
-                    context: 'public',
-                    emitFile: true
-                }
-            }
-        ]
-    }
-});
+// mix.webpackConfig({
+//     output: {
+//         path: path.resolve(__dirname, 'webroot/Public'),
+//         pathinfo: process.env.NODE_ENV !== 'production'
+//         // publicPath: path.normalize('webroot/Public')
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.(woff2?|ttf|eot|svg|otf)$/,
+//                 loader: 'file-loader',
+//                 options: {
+//                     name: '[path][name].[ext]?[hash]',
+//                     outputPath: '/fonts/',
+//                     context: 'public',
+//                     emitFile: true
+//                 }
+//             }
+//         ]
+//     }
+// });
 
-console.log('normalized webroot/Public = ', path.normalize('webroot/Public'));
-console.log('resolved webroot/Public = ', path.resolve(__dirname, 'webroot/Public'));
+// console.log('normalized webroot/Public = ', path.normalize('webroot/Public'));
+// console.log('resolved webroot/Public = ', path.resolve(__dirname, 'webroot/Public'));
 
 mix.sass('resources/sass/app.scss', 'css')
     .js('resources/js/app.js', 'js')

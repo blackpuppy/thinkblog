8525fa9c38323f4050fb7565d114e1510dd06216
Zhu Ming <mingzhu.z+github@gmail.com>
2017-09-01 09:02:02 +0800

    Azure Website
    - 数据库迁移和预置数据：解析 Azure MySQL in App 数据库连接字符串

--- _phinx/phinx.php
+++ _phinx/phinx.php
@@ -8,6 +8,30 @@ if (file_exists($envFilePath)) {
                   ->putenv(true);
 }
 
+$isAzure = false;
+$dbHost = "";
+$dbUser = "";
+$dbPwd = "";
+
+// 解析 Azure MySQL in App 数据库连接字符串
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+        continue;
+    }
+
+    $isAzure = true;
+    $dbHost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    // $dbName = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $dbUser = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $dbPwd = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+if (!$isAzure) {
+    $dbHost = getenv('DB_HOST');
+    $dbUser = getenv('DB_NAME');
+    $dbPwd = getenv('DB_PWD');
+}
+
 date_default_timezone_set(getenv('DEFAULT_TIMEZONE'));
 
 return [
@@ -20,10 +44,10 @@ return [
         'default_migration_table' => 'phinxlog',
         'default' => [
             'adapter'       => getenv('DB_TYPE'),
-            'host'          => getenv('DB_HOST'),
+            'host'          => $dbHost,
             'name'          => getenv('DB_NAME'),
-            'user'          => getenv('DB_USER'),
-            'pass'          => getenv('DB_PWD'),
+            'user'          => $dbUser,
+            'pass'          => $dbPwd,
             'port'          => getenv('DB_PORT'),
             'charset'       => getenv('DB_CHARSET'),
             'collation'     => getenv('DB_COLLATION'),

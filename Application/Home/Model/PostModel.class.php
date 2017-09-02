b610032c01b3f57072125d5a2460c619ff8e5cf9
Zhu Ming <mingzhu.z+github@gmail.com>
2017-09-02 11:20:29 +0800

    数据关联
    - 写文章时，同时保存文章作者

--- Application/Home/Model/PostModel.class.php
+++ Application/Home/Model/PostModel.class.php
@@ -9,4 +9,21 @@ class PostModel extends BaseModel
         ['title', 'require', '{%TITLE_REQUIRED}'],
         ['content', 'require', '{%CONTENT_REQUIRED}'],
     ];
+
+    public function __construct($name = '', $tablePrefix = '', $connection = '')
+    {
+        parent::__construct($name, $tablePrefix, $connection);
+
+        $this->_auto = [
+            ['author_user_id', 'getCurrentUserId', self::MODEL_INSERT, 'callback'],
+        ];
+    }
+
+    public function getCurrentUserId()
+    {
+        $isAuthenticated = session('?authentication.authenticated')
+                        && session('authentication.authenticated');
+        $currentUserId = $isAuthenticated ? session('authentication.user')['id'] : 0;
+        return $currentUserId;
+    }
 }

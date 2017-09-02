5e0239f45eba0d9a461e4165b53d59cfd4a60e69
Zhu Ming <mingzhu.z+github@gmail.com>
2017-09-02 23:38:18 +0800

    数据关联
    - 增加查看用户个人资料页面

--- Application/Home/Controller/ProfileController.class.php
+++ Application/Home/Controller/ProfileController.class.php
@@ -7,7 +7,51 @@ use Think\Model;
 class ProfileController extends Controller
 {
     /**
-     * 修改用户资料。
+     * 查看用户个人资料。
+     * @return void
+     */
+    public function view()
+    {
+        if (!IS_GET) {
+            $this->redirect(U('/'));
+            return;
+        }
+
+        $Profile = D('Profile');
+        $userId = $Profile->getCurrentUserId();
+        if (!$userId) {
+            $this->redirect(U('/'));
+            return;
+        }
+
+        $msg = PHP_EOL . 'Home\Controller\ProfileController::view():'
+            . PHP_EOL . '  $userId = ' . $userId
+            . PHP_EOL . '  REQUEST_METHOD = ' . REQUEST_METHOD;
+
+        $this->assign('title', L('EDIT_PROFILE'));
+
+        try {
+            $profile = $Profile->where(['user_id' => $userId])->find();
+
+            $msg .= PHP_EOL . '  $profile = ' . print_r($profile, true);
+
+            if (!$profile) {
+                $profile = $Profile->create();
+            }
+
+            $this->assign('profile', $profile);
+            $this->display();
+        } catch (Exception $e) {
+            $msg .= PHP_EOL . 'error: ' . $e->getMessage();
+            throw $e;
+        } finally {
+            $msg .= PHP_EOL . str_repeat('-', 80);
+            \Think\Log::write($msg, 'DEBUG');
+        }
+    }
+
+    /**
+     * 修改用户个人资料。
      * @return void
      */
     public function edit()

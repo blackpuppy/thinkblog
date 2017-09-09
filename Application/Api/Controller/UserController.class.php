96baf8978644b387ee4bcb388015cdbf1d4cb011
Zhu Ming <mingzhu.z+github@gmail.com>
2017-09-09 23:40:29 +0800

    Web API
    - 更新 README 和首页

--- Application/Api/Controller/UserController.class.php
+++ Application/Api/Controller/UserController.class.php
@@ -10,6 +10,62 @@ use Api\Controller\BaseController;
 class UserController extends BaseController
 {
     /**
+     * 注册新用户。
+     * @return void
+     */
+    public function signup()
+    {
+        if (!IS_GET && !IS_POST) {
+            $this->redirect(U('/signup'));
+            return;
+        }
+
+        $msg = PHP_EOL . 'Api\Controller\UserController::signup():';
+
+        try {
+            $User = D('Home/User');
+
+            $newUser = $User->create();
+
+            $msg .= PHP_EOL . '  $newUser = ' . print_r($newUser, true);
+
+            if (!$newUser) {
+                $msg .= PHP_EOL . '  validation error: ' . $User->getError();
+
+                $data = [
+                    'user' => I('post.'),
+                    'validationError' => $User->getError(),
+                ];
+                $this->assign($data);
+
+                $this->display();
+            } else {
+                $result = $User->add();
+
+                $msg .= PHP_EOL . '  $result = ' . print_r($result, true);
+
+                if ($result !== false) {
+                    $newUser['id'] = $result;
+
+                    // set authenticated
+                    session('authentication.authenticated', true);
+                    session('authentication.user', $newUser);
+
+                    $this->success(L('SIGNUP_USER_SUCCESS'), U('/'), 3);
+                } else {
+                    $this->error(L('SIGNUP_USER_FAILURE'), U('/signup'), 5);
+                }
+            }
+        } catch (Exception $e) {
+            $msg .= PHP_EOL . '  error: ' . $e->getMessage();
+            throw $e;
+        } finally {
+            $msg .= PHP_EOL . str_repeat('-', 80);
+            \Think\Log::write($msg, 'DEBUG');
+        }
+    }
+
+    /**
      * 登录。
      * @return void
      */
@@ -84,5 +140,18 @@ class UserController extends BaseController
             $msg .= PHP_EOL . str_repeat('-', 80);
             // \Think\Log::write($msg, 'DEBUG');
         }
+    }
+
+    /**
+     * 注销。
+     * @return void
+     */
+    public function logout()
+    {
+        // clear authenticated
+        session('authentication.authenticated', false);
+        session('authentication.user', null);
+
+        $this->redirect(U('/'));
     }
 }

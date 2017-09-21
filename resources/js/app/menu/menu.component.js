c6f89994ba1ce1633c1fa0b20cf8464161fc3c64
Zhu Ming <mingzhu.z+github@gmail.com>
2017-09-21 16:05:37 +0800

    AngularJS 1 客户端
    - 注销

--- resources/js/app/menu/menu.component.js
+++ resources/js/app/menu/menu.component.js
@@ -3,8 +3,8 @@
 angular.module('menu')
 .component('menu', {
     templateUrl: 'Public/template/menu/menu.template.html',
-    controller: ['Auth', '$rootScope', '$log',
-        function(Auth, $rootScope, $log) {
+    controller: ['Auth', '$rootScope', '$state', '$log',
+        function(Auth, $rootScope, $state, $log) {
             this.homeUrl = ThinkBlog.getUrl(ThinkBlog.URL_HOME_PAGE);
             this.postListUrl = ThinkBlog.getUrl(ThinkBlog.URL_POST_LIST);
             this.angularjsUrl = ThinkBlog.getUrl(ThinkBlog.URL_ANGULARJS);
@@ -14,6 +14,11 @@ angular.module('menu')
                     return user.profile.first_name + ' ' + user.profile.last_name;
                 }
                 return null;
+            }
+
+            this.logOut = function() {
+                Auth.ClearCredentials();
+                $state.go('home');
             }
         }
     ]
